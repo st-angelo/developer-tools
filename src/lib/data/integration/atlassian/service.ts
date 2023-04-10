182bfa59ac4f670ab45840bfc3a897d0b34ed201
AngeloStatescu <35111313+AngeloStatescu@users.noreply.github.com>
2023-04-10 17:15:20 +0300

    netlify adapter

--- src/lib/data/integration/atlassian/service.ts
+++ src/lib/data/integration/atlassian/service.ts
@@ -1,5 +1,3 @@
-import { ATLASSIAN_EMAIL, ATLASSIAN_TOKEN } from '$env/static/private';
-import { PUBLIC_ATLASSIAN_CLOUD_URL } from '$env/static/public';
 import type { OperationResponse } from '$lib/data/types/general';
 import IssueCache from '$lib/stores/issueCacheStore';
 import PojoUtil from '$lib/utils/pojo';
@@ -10,7 +8,6 @@ export async function getIssue(key: string): Promise<OperationResponse<Atlassian
 	let cachedIssue = IssueCache.get(key);
 	if (!cachedIssue) {
 		try {
-			console.log(ATLASSIAN_EMAIL, ATLASSIAN_TOKEN, PUBLIC_ATLASSIAN_CLOUD_URL);
 			const issue = await client.issues.getIssue({ issueIdOrKey: key });
 			cachedIssue = IssueCache.set(PojoUtil.getIssueDto(issue));
 		} catch (err) {
